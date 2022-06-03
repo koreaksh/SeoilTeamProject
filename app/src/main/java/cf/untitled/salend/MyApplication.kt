0d0be42731ae15e7a3554a3f95bdf554a5f0e5cd
kkarrkid7 <kkarrkid7@gmail.com>
2022-06-03 21:34:07 +0900

    Using FB, Thread in LikeFragment

--- app/src/main/java/cf/untitled/salend/MyApplication.kt
+++ app/src/main/java/cf/untitled/salend/MyApplication.kt
@@ -11,76 +11,91 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import io.reactivex.disposables.CompositeDisposable
 
-class MyApplication: MultiDexApplication() {
+class MyApplication : MultiDexApplication() {
     companion object {
 
         val auth = Firebase.auth
+
         @SuppressLint("StaticFieldLeak")
         lateinit var db: FirebaseFirestore
         var disposables = CompositeDisposable()
         var email: String? = null
-        lateinit var sharedPref:SharedPreferences
-        lateinit var edit : SharedPreferences.Editor
-        var current_user_email : String? = null
+        lateinit var sharedPref: SharedPreferences
+        lateinit var edit: SharedPreferences.Editor
+        var current_user_email: String? = null
         private var storeFavorite = ArrayList<String?>()
         private var productFavorite = ArrayList<String?>()
+        private var status: Boolean = false
+
+        fun getStatus(): Boolean {
+            return status
+        }
+
+        fun setStatus(): Boolean {
+            this.status = checkAuth()
+            if(this.status)
+                this.current_user_email = auth.currentUser?.email
+            return this.status
+        }
 
         fun checkAuth(): Boolean {     //이메일 인증 완료해야만 true 반환
             val currentUser = auth.currentUser
             return currentUser?.let {  //유저가 저장되어있는지
                 email = currentUser.email
                 currentUser.isEmailVerified
-            } ?: let{
+            } ?: let {
                 false
             }
         }
 
         @JvmName("getStoreFavorite1")
-        fun getStoreFavorite() : ArrayList<String?>{
+        fun getStoreFavorite(): ArrayList<String?> {
             db = FirebaseFirestore.getInstance()
             val docRef = db.collection("profile")
-
-            val task = docRef.whereEqualTo("u_id", current_user_email!!).get()
+            Log.e("TAG", "getStoreFavorite: " + docRef + (docRef == null))
+            val task = docRef
+                .whereEqualTo("u_id", current_user_email!!)
+                .get()
             var asd = Tasks.await(task)
             val uStore = asd.documents[0].data?.get("u_store_favorite") as ArrayList<String?>?
-            if( uStore!= null)
+            if (uStore != null)
                 storeFavorite = uStore
             return storeFavorite
         }
 
         @JvmName("getProductFavorite1")
-        fun getProductFavorite(): ArrayList<String?>{
+        fun getProductFavorite(): ArrayList<String?> {
             db = FirebaseFirestore.getInstance()
             val docRef = db.collection("profile")
             val task = docRef.whereEqualTo("u_id", current_user_email!!).get()
             var asd = Tasks.await(task)
             val uProduct = asd.documents[0].data?.get("u_item_favorite") as ArrayList<String?>?
-            if(uProduct != null)
+            if (uProduct != null)
                 productFavorite = uProduct
 
             return productFavorite
         }
 
-        fun setStoreFavorite(userId : String, productId : String) {
+        fun setStoreFavorite(userId: String, productId: String) {
             db = FirebaseFirestore.getInstance()
             var lastStoreFavorite = getStoreFavorite()
             lastStoreFavorite.add(productId)
             db.collection("profile").document(userId).update("u_store_favorite", lastStoreFavorite)
         }
 
-        fun setProductFavorite(userId : String, productId : String) {
+        fun setProductFavorite(userId: String, productId: String) {
             db = FirebaseFirestore.getInstance()
             var lastProductFavorite = getProductFavorite()
             lastProductFavorite.add(productId)
             db.collection("profile").document(userId).update("u_item_favorite", lastProductFavorite)
         }
 
-        fun updateStoreFavorite(userId: String, storeFavorite: ArrayList<String?>){
+        fun updateStoreFavorite(userId: String, storeFavorite: ArrayList<String?>) {
             db = FirebaseFirestore.getInstance()
-            db.collection("profile").document(userId).update("u_store_favorite",storeFavorite)
+            db.collection("profile").document(userId).update("u_store_favorite", storeFavorite)
         }
 
-        fun updateProductFavorite(userId: String, productFavorite: ArrayList<String?>){
+        fun updateProductFavorite(userId: String, productFavorite: ArrayList<String?>) {
             db = FirebaseFirestore.getInstance()
             db.collection("profile").document(userId).update("u_item_favorite", productFavorite)
         }
@@ -91,17 +106,17 @@ class MyApplication: MultiDexApplication() {
             updateStoreFavorite(userId, lastStoreFavorite)
         }
 
-        fun delProductFavorite(userId : String, productId: String){
+        fun delProductFavorite(userId: String, productId: String) {
             val lastProductFavorite = this.getProductFavorite()
             lastProductFavorite.removeAll(listOf(productId))
             updateProductFavorite(userId, lastProductFavorite)
         }
 
-        fun checkStoreFavorite(storeId: String):Boolean {
+        fun checkStoreFavorite(storeId: String): Boolean {
             return this.getStoreFavorite().contains(storeId)
         }
 
-        fun checkProductFavorite(productId: String):Boolean{
+        fun checkProductFavorite(productId: String): Boolean {
             return this.getProductFavorite().contains(productId)
         }
 
--- app/src/main/java/cf/untitled/salend/fragment/LikeFragment.kt
+++ app/src/main/java/cf/untitled/salend/fragment/LikeFragment.kt
@@ -1,16 +1,24 @@
 package cf.untitled.salend.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import cf.untitled.salend.MyApplication
 import cf.untitled.salend.R
+import cf.untitled.salend.databinding.FragmentLikeBinding
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.concurrent.thread
 
 // TODO: Rename parameter arguments, choose names that match
 // the cf.untitled.salend.fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private lateinit var favArray: ArrayList<String?>
+private var initFlag: Boolean = false;
 
 /**
  * A simple [Fragment] subclass.
@@ -28,6 +36,39 @@ class LikeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        initFlag = true;
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val TAG = "asdf"
+        val f: FragmentLikeBinding = FragmentLikeBinding.bind(view)
+        val thread = Thread {
+            run {
+                Log.e(TAG, "LF: " + 1 + f.favoritePageStoreButton.text)
+                f.favoritePageStoreButton.text = "A"
+                Log.e(TAG, "LF: " + MyApplication.getStatus())
+                if (MyApplication.setStatus()) {
+                    favArray = MyApplication.getStoreFavorite()
+                    Log.e(TAG, "LF: " + MyApplication.getStoreFavorite())
+                }
+            }
+        }
+
+        f.favoritePageItemButton.setOnClickListener {
+            f.favoritePageStoreButton.text = "asdf"
+        }
+
+        Log.e(TAG, "LF: " + thread.state)
+        if (thread.state == Thread.State.NEW)
+            thread.start()
+    }
+
+    public fun refreshList() {
+        if (MyApplication.setStatus()) {
+            favArray = MyApplication.getStoreFavorite()
+            Log.e("asdf", "LF: " + MyApplication.getStoreFavorite())
+        }
     }
 
     override fun onCreateView(
@@ -36,6 +77,18 @@ class LikeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this cf.untitled.salend.fragment
         return inflater.inflate(R.layout.fragment_like, container, false)
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        if (initFlag) {
+            Thread {
+                run() {
+                    refreshList()
+                    Log.e("asdf", "setUserVisibleHint: " + favArray)
+                }
+            }.start()
+        }
     }
 
     companion object {
